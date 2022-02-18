c872ab0a3aa7a06f2f5235979485f766b42ce823
Ian <lai.kheng.yan@gmail.com>
2022-02-18 17:59:01 +0800

    i

--- Assignment 3/Q8.c
+++ Assignment 3/Q8.c
@@ -27,13 +27,13 @@ int main() {
     BTNode *root = NULL;
     buildTree(&root, preO, postO);
     if (root == NULL) printf("error\n");
-    preOrder(root);
-    printf("\n");
-    postOrder(root);
-    printf("\n");
-
-    inOrder(root);
-    printf("\n");
+//    preOrder(root);
+//    printf("\n");
+//    postOrder(root);
+//    printf("\n");
+//
+//    inOrder(root);
+//    printf("\n");
 
     return 0;
 }
@@ -70,42 +70,79 @@ void postOrder(BTNode *cur) {
 void buildTree(BTNode **node, char *preO, char *postO) {
     // generate binary tree from preorder and postorder
 
+    if (preO[0] == '\0' || postO[0] == '\0') {
+        printf("TRUE");
+//        *node = NULL;
+        return;
+    }
+
+    if(strlen(preO) == 1 && strlen(postO) == 1) {
+//        *node = (BTNode *) malloc(sizeof(BTNode));
+//        (*node)->id = preO[0];
+//        (*node)->left = NULL;
+//        (*node)->right = NULL;
+//printf("NODE%c\n", preO[0]);
+        return;
+    }
+
+
     printf("ROOT: %c\n", preO[0]);
 
     char *templeftpreO = malloc(sizeof(char) * MAX_N);
     int templeftindex = 0;
-
-    char *temprightpostO = malloc(sizeof(char) * MAX_N);
+    char *templeftpostO = malloc(sizeof(char) * MAX_N);
+    int templeftpostoindex = 0;
+    char *temprightpreO = malloc(sizeof(char) * MAX_N);
     int temprightindex = 0;
+    char *temprightpostO = malloc(sizeof(char) * MAX_N);
+    int temprightpostoindex = 0;
 
     while (preO[templeftindex] != postO[strlen(postO) - 2]) {
-        templeftpreO[templeftindex] = preO[templeftindex+1];
+        templeftpreO[templeftindex] = preO[templeftindex + 1];
         templeftindex++;
     }
+    templeftpreO[templeftindex - 1] = '\0';
+    printf("LEFTpreO: %s\n", templeftpreO);
 
-    templeftpreO[templeftindex-1] = '\0';
-    printf("%d\n", templeftindex);
+    while (postO[templeftpostoindex] != preO[1]) {
+        templeftpostO[templeftpostoindex] = postO[templeftpostoindex];
+        templeftpostoindex++;
+    }
 
-    while (preO[templeftindex] != preO[strlen(preO)-1]) {
-        printf("%c ", preO[templeftindex]);
-        printf("%c", preO[strlen(preO)-1]);
-        printf("\n");
-        temprightpostO[temprightindex] = preO[templeftindex];
+    templeftpostO[templeftpostoindex] = preO[1];
+    templeftpostO[templeftpostoindex + 1] = '\0';
+    printf("LEFTposto: %s\n", templeftpostO);
+
+
+    while (preO[templeftindex] != preO[strlen(preO) - 1]) {
+
+        temprightpreO[temprightindex] = preO[templeftindex];
         templeftindex++;
         temprightindex++;
     }
 
 
-    temprightpostO[temprightindex] = '\0';
+    temprightpreO[temprightindex] = preO[strlen(preO) - 1];
+    temprightpreO[temprightindex + 1] = '\0';
+    printf("RIGHTpreo: %s\n", temprightpreO);
 
-    printf("LEFT: %s\n", templeftpreO);
-    printf("RIGHT: %s\n", temprightpostO);
+    templeftpostoindex++;
+    while (postO[templeftpostoindex] != postO[strlen(postO) - 1]) {
+        temprightpostO[temprightpostoindex] = postO[templeftpostoindex];
+        temprightpostoindex++;
+        templeftpostoindex++;
+    }
+
+
+    temprightpostO[temprightpostoindex] = '\0';
+
+    printf("RIGHTposto: %s\n", temprightpostO);
+
+    buildTree(NULL, templeftpreO, templeftpostO);
+    buildTree(NULL, temprightpreO, temprightpostO);
+
 
 }
-
-
-
-
 
 
 
