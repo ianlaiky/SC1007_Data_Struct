3ca9939fd40dd3bda3d2f1b8b33193c0eadeb33f
Ian <lai.kheng.yan@gmail.com>
2022-02-18 14:52:53 +0800

    i

---
+++ Assignment 3/Q8.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_N 120
+
+typedef struct _btnode{
+    char id;
+    struct _btnode *left;
+    struct _btnode *right;
+} BTNode;
+
+void buildTree(BTNode** node, char* preO, char* postO);
+void inOrder(BTNode *cur);
+void preOrder(BTNode *cur);
+void postOrder(BTNode *cur);
+
+int main()
+{
+    char preO[MAX_N];
+    char postO[MAX_N];
+    scanf("%s",preO);
+    scanf("%s",postO);
+
+    BTNode* root=NULL;
+    buildTree(&root,preO,postO);
+    if(root==NULL) printf("error\n");
+    preOrder(root);
+    printf("\n");
+    postOrder(root);
+    printf("\n");
+
+    inOrder(root);
+    printf("\n");
+
+    return 0;
+}
+void inOrder(BTNode *cur){
+    if (cur == NULL)
+        return;
+
+    inOrder(cur->left);
+    printf("%c",cur->id);
+    inOrder(cur->right);
+}
+void preOrder(BTNode *cur){
+    if (cur == NULL)
+        return;
+
+    printf("%c",cur->id);
+    preOrder(cur->left);
+    preOrder(cur->right);
+}
+void postOrder(BTNode *cur){
+    if (cur == NULL)
+        return;
+
+    postOrder(cur->left);
+    postOrder(cur->right);
+    printf("%c",cur->id);
+}
+
+void buildTree(BTNode** node, char* preO, char* postO)
+{
+
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+
+// Write your code here
+}
