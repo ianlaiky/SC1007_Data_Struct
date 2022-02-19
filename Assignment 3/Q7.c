cbd95a3900375791a50d07dc98fa52de2f615962
Ian <lai.kheng.yan@gmail.com>
2022-02-19 22:45:08 +0800

    i

--- Assignment 3/Q7.c
+++ Assignment 3/Q7.c
@@ -1,83 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _btnode{
+typedef struct _btnode {
     int nodeV;
     struct _btnode *left;
     struct _btnode *right;
 } BTNode;
 
-typedef struct _queuenode{
-    BTNode* node;
+typedef struct _queuenode {
+    BTNode *node;
     struct _queuenode *next;
-}QueueNode;
+} QueueNode;
 
-typedef struct _queue{
+typedef struct _queue {
     int size;
     QueueNode *head;
     QueueNode *tail;
 } Queue;
 
-void printBTNode(BTNode *root, int space,int left);
+void printBTNode(BTNode *root, int space, int left);
+
 //Prototypes of Interface functions for Queue structure
-void enqueue(Queue *qPtr, BTNode * node);
+void enqueue(Queue *qPtr, BTNode *node);
+
 int dequeue(Queue *qPtr);
-BTNode* getFront(Queue q);
+
+BTNode *getFront(Queue q);
+
 int isEmptyQueue(Queue q);
 
-int twoNodesCost(BTNode* node, int nodeV1,int nodeV2);
+int twoNodesCost(BTNode *node, int nodeV1, int nodeV2);
 
-int main()
-{
-    BTNode* root = (BTNode*) malloc(sizeof(BTNode));
+int main() {
+    BTNode *root = (BTNode *) malloc(sizeof(BTNode));
 
     Queue q;
     q.head = NULL;
     q.tail = NULL;
     q.size = 0;
 
-    BTNode* node;
-    enqueue(&q,root);
+    BTNode *node;
+    enqueue(&q, root);
 
     int nodeV;
     char opL, opR;
 
-    while(!isEmptyQueue(q)){
-        scanf("%d %c %c",&nodeV,&opL,&opR);
-        node = getFront(q);dequeue(&q);
+    while (!isEmptyQueue(q)) {
+        scanf("%d %c %c", &nodeV, &opL, &opR);
+        node = getFront(q);
+        dequeue(&q);
         node->nodeV = nodeV;
 
-        if(opL != '@'){
-            node->left = (BTNode*) malloc(sizeof(BTNode));
-            enqueue(&q,node->left);
-        }
-        else node->left =NULL;
-        if(opR != '@'){
-            node->right = (BTNode*) malloc(sizeof(BTNode));
-            enqueue(&q,node->right);
-        }
-        else
+        if (opL != '@') {
+            node->left = (BTNode *) malloc(sizeof(BTNode));
+            enqueue(&q, node->left);
+        } else node->left = NULL;
+        if (opR != '@') {
+            node->right = (BTNode *) malloc(sizeof(BTNode));
+            enqueue(&q, node->right);
+        } else
             node->right = NULL;
     }
 
-    int v1,v2;
-    scanf("%d %d",&v1,&v2);
-    int cost = twoNodesCost(root,v1,v2);
+    int v1, v2;
+    scanf("%d %d", &v1, &v2);
+    int cost = twoNodesCost(root, v1, v2);
 
-    printBTNode(root,0,0);
+    printBTNode(root, 0, 0);
 
-    printf("Distance is %d\n",cost);
+    printf("Distance is %d\n", cost);
     return 0;
 }
 
-void enqueue(Queue *qPtr, BTNode *node){
+void enqueue(Queue *qPtr, BTNode *node) {
     QueueNode *newNode;
     newNode = malloc(sizeof(QueueNode));
     newNode->node = node;
     newNode->next = NULL;
 
-    if(isEmptyQueue(*qPtr))
-        qPtr->head=newNode;
+    if (isEmptyQueue(*qPtr))
+        qPtr->head = newNode;
     else
         qPtr->tail->next = newNode;
 
@@ -85,14 +87,13 @@ void enqueue(Queue *qPtr, BTNode *node){
     qPtr->size++;
 }
 
-int dequeue(Queue *qPtr){
-    if(qPtr==NULL || qPtr->head==NULL){ //Queue is empty or NULL pointer
+int dequeue(Queue *qPtr) {
+    if (qPtr == NULL || qPtr->head == NULL) { //Queue is empty or NULL pointer
         return 0;
-    }
-    else{
+    } else {
         QueueNode *temp = qPtr->head;
         qPtr->head = qPtr->head->next;
-        if(qPtr->head == NULL) //Queue is emptied
+        if (qPtr->head == NULL) //Queue is emptied
             qPtr->tail = NULL;
 
         free(temp);
@@ -101,25 +102,25 @@ int dequeue(Queue *qPtr){
     }
 }
 
-BTNode* getFront(Queue q){
+BTNode *getFront(Queue q) {
     return q.head->node;
 }
 
-int isEmptyQueue(Queue q){
-    if(q.size==0) return 1;
+int isEmptyQueue(Queue q) {
+    if (q.size == 0) return 1;
     else return 0;
 }
-void printBTNode(BTNode *root,int space,int left){
-    if (root != NULL)
-    {
+
+void printBTNode(BTNode *root, int space, int left) {
+    if (root != NULL) {
 
         int i;
-        for (i = 0; i < space-1; i++)
+        for (i = 0; i < space - 1; i++)
             printf("|\t");
 
 
-        if(i<space){
-            if(left==1)
+        if (i < space) {
+            if (left == 1)
                 printf("|---");
             else
                 printf("|___");
@@ -128,16 +129,15 @@ void printBTNode(BTNode *root,int space,int left){
         printf("%d\n", root->nodeV);
 
         space++;
-        printBTNode(root->left, space,1);
-        printBTNode(root->right, space,0);
+        printBTNode(root->left, space, 1);
+        printBTNode(root->right, space, 0);
     }
 }
 
-int twoNodesCost(BTNode* node, int nodeV1,int nodeV2)
-{
+int twoNodesCost(BTNode *node, int nodeV1, int nodeV2) {
 
 // Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-    */
+    * /
 
 // Write your code here
 }
