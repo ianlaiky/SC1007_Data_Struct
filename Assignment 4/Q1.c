bafa39fdab6dd98cfc244b28007b7d51862961f3
ian <lai.kheng.yan@gmail.com>
2022-02-23 18:35:23 +0800

    i

--- Assignment 4/Q1.c
+++ Assignment 4/Q1.c
@@ -1,88 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _btnode{
+typedef struct _btnode {
     int item;
     struct _btnode *left;
     struct _btnode *right;
 } BTNode;
 
-typedef struct _queueNode{
+typedef struct _queueNode {
     BTNode *data;
     struct _queueNode *next;
-}QueueNode;
+} QueueNode;
 
-typedef struct _queue{
+typedef struct _queue {
     int size;
     QueueNode *head;
     QueueNode *tail;
 } Queue;
 
 //Prototypes of Interface functions for Queue structure
-void enqueue(Queue *qPtr, BTNode * data);
+void enqueue(Queue *qPtr, BTNode *data);
+
 int dequeue(Queue *qPtr);
-BTNode* getFront(Queue q);
+
+BTNode *getFront(Queue q);
+
 int isEmptyQueue(Queue q);
 
-BTNode* createNode(int item);
+BTNode *createNode(int item);
+
 void printBTNode(BTNode *root, int space, int left);
+
 void deleteTree(BTNode **root);
 
-BTNode* findLargestBST (BTNode* root);
+BTNode *findLargestBST(BTNode *root);
 
-int main()
-{
-    BTNode* root = NULL;
-    BTNode* temp = NULL;
+int main() {
+    BTNode *root = NULL;
+    BTNode *temp = NULL;
     Queue q;
     q.head = NULL;
     q.tail = NULL;
     q.size = 0;
 
     int item;
-    int flag=1;
+    int flag = 1;
     printf("Enter a list of numbers for a Binary Tree, terminated by any non-digit character: \n");
-    while(scanf("%d",&item))
-    {
-        if(root==NULL)
-        {
-            if(item!=-1){
-                root =createNode(item);
-                enqueue(&q,root);
+    while (scanf("%d", &item)) {
+        if (root == NULL) {
+            if (item != -1) {
+                root = createNode(item);
+                enqueue(&q, root);
             }
-        }
-        else{
-            while(!isEmptyQueue(q)){
+        } else {
+            while (!isEmptyQueue(q)) {
                 temp = getFront(q);
-                if(flag){
-                    if(item != -1){
+                if (flag) {
+                    if (item != -1) {
                         temp->left = createNode(item);
-                        enqueue(&q,temp->left);
+                        enqueue(&q, temp->left);
                     }
                     flag = 0;
                     break;
-                }
-                else if(temp->right==NULL){
-                    if(item!=-1){
+                } else if (temp->right == NULL) {
+                    if (item != -1) {
                         temp->right = createNode(item);
-                        enqueue(&q,temp->right);
+                        enqueue(&q, temp->right);
                     }
                     dequeue(&q);
-                    flag=1;
+                    flag = 1;
                     break;
-                }
-                else  dequeue(&q);
+                } else dequeue(&q);
             }
         }
     }
     scanf("%*s");
     printf("The input binary tree:\n");
-    printBTNode(root,0,0);
+    printBTNode(root, 0, 0);
     printf("\n");
-    BTNode* BSTnode = findLargestBST (root);
+    BTNode *BSTnode = findLargestBST(root);
     printf("The largest binary search subtree:\n");
-    if(BSTnode)
-        printBTNode(BSTnode,0,0);
+    if (BSTnode)
+        printBTNode(BSTnode, 0, 0);
     else
         printf("No BST\n");
     deleteTree(&root);
@@ -90,25 +89,23 @@ int main()
     return 0;
 }
 
-BTNode* createNode(int item)
-{
-    BTNode* node = (BTNode*) malloc(sizeof(BTNode));
+BTNode *createNode(int item) {
+    BTNode *node = (BTNode *) malloc(sizeof(BTNode));
     node->item = item;
     node->left = node->right = NULL;
     return node;
 }
 
-void printBTNode(BTNode *root,int space,int left){
-    if (root != NULL)
-    {
+void printBTNode(BTNode *root, int space, int left) {
+    if (root != NULL) {
 
         int i;
-        for (i = 0; i < space-1; i++)
+        for (i = 0; i < space - 1; i++)
             printf("|\t");
 
 
-        if(i<space){
-            if(left==1)
+        if (i < space) {
+            if (left == 1)
                 printf("|---");
             else
                 printf("|___");
@@ -117,28 +114,28 @@ void printBTNode(BTNode *root,int space,int left){
         printf("%d\n", root->item);
 
         space++;
-        printBTNode(root->left, space,1);
-        printBTNode(root->right, space,0);
+        printBTNode(root->left, space, 1);
+        printBTNode(root->right, space, 0);
     }
 }
 
-void deleteTree(BTNode **root){
-    if (*root != NULL)
-    {
+void deleteTree(BTNode **root) {
+    if (*root != NULL) {
         deleteTree(&((*root)->left));
         deleteTree(&((*root)->right));
         free(*root);
         *root = NULL;
     }
 }
-void enqueue(Queue *qPtr, BTNode *data){
+
+void enqueue(Queue *qPtr, BTNode *data) {
     QueueNode *newNode;
     newNode = malloc(sizeof(QueueNode));
     newNode->data = data;
     newNode->next = NULL;
 
-    if(isEmptyQueue(*qPtr))
-        qPtr->head=newNode;
+    if (isEmptyQueue(*qPtr))
+        qPtr->head = newNode;
     else
         qPtr->tail->next = newNode;
 
@@ -146,14 +143,13 @@ void enqueue(Queue *qPtr, BTNode *data){
     qPtr->size++;
 }
 
-int dequeue(Queue *qPtr){
-    if(qPtr==NULL || qPtr->head==NULL){ //Queue is empty or NULL pointer
+int dequeue(Queue *qPtr) {
+    if (qPtr == NULL || qPtr->head == NULL) { //Queue is empty or NULL pointer
         return 0;
-    }
-    else{
+    } else {
         QueueNode *temp = qPtr->head;
         qPtr->head = qPtr->head->next;
-        if(qPtr->head == NULL) //Queue is emptied
+        if (qPtr->head == NULL) //Queue is emptied
             qPtr->tail = NULL;
 
         free(temp);
@@ -162,23 +158,63 @@ int dequeue(Queue *qPtr){
     }
 }
 
-BTNode* getFront(Queue q){
+BTNode *getFront(Queue q) {
     return q.head->data;
 }
 
-int isEmptyQueue(Queue q){
-    if(q.size==0) return 1;
+int isEmptyQueue(Queue q) {
+    if (q.size == 0) return 1;
     else return 0;
 }
 
 
-BTNode* findLargestBST (BTNode* root)
-{
-// Write your code here
-
-
-
-
-
-
+int countNodes(BTNode *root) {
+    if (root == NULL) return 0;
+    else return 1 + countNodes(root->left) + countNodes(root->right);
 }
+int min =-1;
+// check if tree has a valid BST
+int isBST(BTNode *root) {
+
+    if (root == NULL) return 1;
+
+    if (root->left != NULL && root->left->item > root->item && root->left->item < min){
+        return 0;
+    }
+    if (root->right != NULL && root->right->item < root->item && root->right->item > min){
+        return 0;
+    }
+//    if(root->item<min && min!=-1){
+//        return 0;
+//    }
+    min = root->item;
+    printf("%d\n", min);
+    return isBST(root->left) && isBST(root->right);
+}
+
+
+BTNode *findLargestBST(BTNode *root) {
+
+
+    BTNode *temp = root;
+
+    while (temp->right != NULL || temp->left != NULL) {
+        printf("bst:%d item: %d \n", isBST(temp),temp->item);
+        if (isBST(temp)) {
+            break;
+        } else {
+            int leftcount = countNodes(temp->left);
+            int rightcoun = countNodes(temp->right);
+            if (leftcount >= rightcoun) {
+                temp = temp->left;
+            } else {
+                temp = temp->right;
+            }
+        }
+    }
+
+
+    return temp;
+}
+
+
