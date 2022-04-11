0082bbebce271d20b63ca5a191abc41e6ccd49bf
ian <lai.kheng.yan@gmail.com>
2022-04-11 20:38:48 +0800

    i

---
+++ Assignment 7&8/Q6.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int top_down_dp(int n) {
+    //write your code here
+}
+
+
+int main() {
+    int n;
+    int function;
+    int i;
+    printf("Enter the value of n:\n");
+    scanf("%d", &n);
+
+    printf("The value of F(%d) is: %d \n", n, top_down_dp(n));
+
+}
