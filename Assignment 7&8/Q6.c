2b1160e68ffef8277cd45abe14aadffd475bf098
ian <lai.kheng.yan@gmail.com>
2022-04-12 01:04:26 +0800

    i

--- Assignment 7&8/Q6.c
+++ Assignment 7&8/Q6.c
@@ -2,9 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int *M;
 int top_down_dp(int n) {
     //write your code here
+    if (n == 0){
+        M[0] = 0;
+        return 0;
+    }
+
+    if (n == 1){
+        M[1] = 1;
+        return 1;
+    }
+
+    if (n == 2){
+        M[2] = 2;
+        return 2;
+    }
+
+
+    if (M[n-1] == -1)			        //F(n-1) was not calculated
+        M[n-1] = top_down_dp(n-1);		//calculate F(n-1) and store in M
+
+    if (M[n-2] == -1)			        //F(n-2) was not calculated
+        M[n-2] = top_down_dp(n-2);		//calculate F(n-2) and store in M
+
+    if (M[n-3] == -1)			        //F(n-3) was not calculated
+        M[n-3] = top_down_dp(n-3);		//calculate F(n-3) and store in M
+    M[n] = M[n-1] + 2*M[n-2] - 3*M[n-3];
+    return M[n];
 }
 
 
@@ -14,7 +40,9 @@ int main() {
     int i;
     printf("Enter the value of n:\n");
     scanf("%d", &n);
-
+    M = malloc(sizeof(int) * (n+1));
+    for (i=0; i<=n; i++)
+        M[i] = -1;
     printf("The value of F(%d) is: %d \n", n, top_down_dp(n));
 
 }
