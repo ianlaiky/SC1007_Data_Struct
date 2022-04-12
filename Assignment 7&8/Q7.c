500d6e617714f946b03335a69c5719c45e1dbf6d
ian <lai.kheng.yan@gmail.com>
2022-04-12 13:14:48 +0800

    i

--- Assignment 7&8/Q7.c
+++ Assignment 7&8/Q7.c
@@ -1,13 +1,22 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+int *M;
 
 
 int bottom_up_dp(int n, int *s, int *v, int C)
 {
     //write your code here
 
+    int i;
 
+//    M[0] = 0;
+//    M[1] = 1;
+
+    for (i=0;i<=n;i++)
+        M[i] = M[i-1] + M[i-2];
+
+    return M[n];
 
 
 }
@@ -22,6 +31,8 @@ int main ()
     int i,j;
     printf("Enter the number of items n:\n");
     scanf("%d",&n);
+    for (i=0; i<=n; i++)
+        M[i] = -1;
     printf("Enter the capacity C:\n");
     scanf("%d",&C);
     s = (int *) malloc(sizeof(int) * (n+1));
